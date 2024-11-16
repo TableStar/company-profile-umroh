578db994c95b01999458ad34c2e94a90a925629f
TableStar <afraroad@gmail.com>
2024-11-16 13:27:10 +0700

    fix: added 4 of products and styling them in mobile and desktop

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-const ProductCard = () => {
+type ProductCard = {
+  src?: string;
+  title?: string;
+  description?: string;
+};
+
+const ProductCard = ({ src, title, description }: ProductCard) => {
   return (
-    <div>ProductCard</div>
-  )
-}
+    <div className="md:h-[514px] md:w-[584px] flex flex-col justify-start items-start gap-2">
+      <div className="h-64 md:h-[400px] w-full relative bg-sky-50 rounded-xl">
+        {src && (
+          <Image
+            src={src}
+            alt="product1"
+            width={800}
+            height={600}
+            className="rounded-xl w-full h-full object-cover"
+          />
+        )}
+      </div>
+      <div className="w-full h-24 flex flex-col justify-start items-start gap-2">
+        <div className="w-full h-24 flex flex-col justify-start items-start gap-1">
+          <h3 className="text-neutral-800 text-lg font-semibold font-['DM_Sans'] leading-7">
+            {title && title}
+          </h3>
+          <p className="w-full text-neutral-600 text-sm font-normal font-['Inter'] leading-[21px]">
+            {description && description}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
--- src/components/home/Products.tsx
+++ src/components/home/Products.tsx
@@ -1,12 +1,40 @@
 import Image from "next/image";
 import GoldSectionHeader from "../GoldSectionHeader";
+import ProductCard from "../ProductCard";
+
+const productList = [
+  {
+    src: "https://picsum.photos/800/600",
+    title: "Umroh Small Group",
+    description:
+      " Umroh Small Group adalah solusi ideal bagi Anda yang ingin melaksanakan ibadah umroh dengan lebih nyaman dan personal.",
+  },
+  {
+    src: "https://picsum.photos/801/600",
+    title: "Umroh Smart Family - Platinum 12D",
+    description:
+      "Umroh Smart Family adalah solusi ideal bagi Anda yang ingin melaksanakan ibadah umroh dengan kenyamanan dan kebersamaan.",
+  },
+  {
+    src: "https://picsum.photos/800/601",
+    title: "Jelajahi Turki 3D2N",
+    description:
+      "Dalam tur ini, Anda akan merasakan pesona megah bangunan bersejarah,  keindahan alam yang menakjubkan, serta keramahan dan kehangatan  masyarakat Turki.",
+  },
+  {
+    src: "https://picsum.photos/801/601",
+    title: "Uzbekistan 8D7N",
+    description:
+      "Pada tur kali ini Anda akan diajak untuk mengenal lebih banyak tentang Uzbekistan serta berbagai pesona yang dimilikinya.",
+  },
+];
 
 const Products = () => {
   return (
     <div className="px-4 md:px-12 py-8 mt-32 md:mt-72 flex flex-col items-center">
       <div className="flex flex-col items-center gap-y-8">
         <GoldSectionHeader title="Produk Kami" />
-        <div className="h-72 flex flex-col justify-start items-center gap-6">
+        <div className="h-72 md:h-auto flex flex-col justify-start items-center gap-6">
           <div className="self-stretch text-center text-[#252525] text-[28px] font-bold font-['Poppins'] leading-[42px]">
             Paket - Paket Terbaik yang Kami Berikan untuk Anda
           </div>
@@ -15,30 +43,21 @@ const Products = () => {
             ibadah dan wisata melalui agensi kami.
           </div>
         </div>
-        <div className="flex flex-col">
-          <div className="h-88 flex flex-col justify-start items-start gap-2">
-            <div className="h-64 w-full relative bg-sky-50 rounded-xl">
-              <Image
-                src="https://picsum.photos/800/600"
-                alt="product1"
-                width={800}
-                height={600}
-                className="rounded-xl w-full h-auto object-cover"
-              />
-            </div>
-            <div className="w-full h-24 flex flex-col justify-start items-start gap-2">
-              <div className="w-full h-24 flex flex-col justify-start items-start gap-1">
-                <h3 className="text-neutral-800 text-lg font-semibold font-['DM_Sans'] leading-7">
-                  Umroh Small Group
-                </h3>
-                <p className="w-full text-neutral-600 text-sm font-normal font-['Inter'] leading-[21px]">
-                  Umroh Small Group adalah solusi ideal bagi Anda yang ingin
-                  melaksanakan ibadah umroh dengan lebih nyaman dan personal.
-                </p>
-              </div>
-            </div>
-          </div>
+        <div className="flex flex-col gap-8 md:grid md:grid-cols-2 md:max-w-[1200px]">
+          {productList.map((product, i) => (
+            <ProductCard
+              key={i}
+              title={product.title}
+              description={product.description}
+              src={product.src}
+            />
+          ))}
         </div>
+        <button className="h-10 px-5 py-3 bg-gray-100 rounded-lg flex justify-center items-center gap-2 hover:bg-gray-200 transition-colors">
+          <span className="text-neutral-800 text-sm font-medium font-['Inter']">
+            Lihat Semua
+          </span>
+        </button>
       </div>
     </div>
   );
